ba218ea2be18585c83432318533eaf308fb58f19
Krzysztof Szumny <kszumny@spistresci.pl>
2013-08-02 20:06:13 +0200

    Fixes #1: Prosty szablon html'a dla django.

--- programuj_z_wykopem/settings.py
+++ programuj_z_wykopem/settings.py
@@ -1,4 +1,5 @@
 # Django settings for programuj_z_wykopem project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -67,11 +68,14 @@ STATIC_ROOT = ''
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
 
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(SITE_ROOT,'static/'),
 )
 
 # List of finder classes that know how to find static files in
@@ -111,6 +115,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(SITE_ROOT,'templates/'),
 )
 
 INSTALLED_APPS = (
